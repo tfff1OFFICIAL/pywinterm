dece842b22d77d8a0d20ee9b244d8cbd418be1eb
Tfff1OFFICIAL <tfff1OFFICIAL@users.noreply.github.com>
2017-06-25 18:17:50 +1200

    fixes

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='pywinterm',
+setup(
+      name='pywinterm',
       version='0.1',
       description='A tiny library for doing some basic terminal control and keyhandling in Python',
       url='https://github.com/tfff1OFFICIAL/pywinterm',
       author='tfff1OFFICIAL',
       author_email='tfff1s.modpacks@gmail.com',
       license='MIT',
-      packages=['pywinterm'],
-      zip_safe=False)
+      packages=find_packages(),
+      zip_safe=False
+)
